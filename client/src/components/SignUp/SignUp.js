6c8810427b69c9c9a6843fa5cabcecf7d3f68168
Ahmedmusa72 <imadormusa72@gmail.com>
2023-03-24 04:14:52 -0700

    created signup,notfound and home component

---
+++ client/src/components/SignUp/SignUp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SignUp.css";
+import Header from "../Partials/Header/Header"
+import Footer from "../Partials/Footer/Footer";
+
+const SignUp = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>signUp page</h1>
+      <Footer/>
+    </div>
+  );
+};
+
+export default SignUp;
